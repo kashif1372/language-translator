b37a7f5470cd4b47e6c9a90a0b0a6702216812c4
Kashif Sheikh <67710001+kashif1372@users.noreply.github.com>
2023-09-20 16:22:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+import { Navbar } from './components/Navbar';
+import { TranslateBox } from './components/TranslateBox';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <TranslateBox />
+      <ToastContainer autoClose={3000} />
+    </div>
+  );
+}
+
+export default App;
